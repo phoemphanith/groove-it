50b8b04b32a4a9a64b2bd5585284509c87d673ef
phoemphanith <phanith1826@gmail.com>
2024-12-31 08:51:43 +0700

    Testing

--- src/hooks/usePlayer.jsx
+++ src/hooks/usePlayer.jsx
@@ -87,6 +87,7 @@ export default function usePlayer() {
   const usePlayerInit = () => {
     useEffect(() => {
       if (currentTrack?.audioSrc) {
+        // https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3
         load(audioSrc, {
           autoplay,
           loop: true,
